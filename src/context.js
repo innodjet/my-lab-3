20af102494818b4c90f08a5199e5245f13039cb9
NIK DJETELI <innodjet@gmail.com>
2019-06-08 17:25:17 -0500

    Update context.js

--- src/context.js
+++ src/context.js
@@ -55,10 +55,11 @@ class PeopleContextProvider extends Component {
                 homeworld: el.homeworld
               };
     });
-    // We have count which is the tital number of result for the API
+    // We have count which is the total number of result for the API
     // We know that the API return 10 result per request then 
     // let's calculate the number of page we have in total
     const totalPage = (parseInt(data.count%10)) === 0 ? parseInt(data.count/10):(parseInt(data.count/10)) + 1;
+    // let 's grab the next page URL to perform a next API call to get the data
     let nextUrl = data.next;
     if ( nextUrl !== null ) {
       for ( let i=1; i <= totalPage; i++ ) {
